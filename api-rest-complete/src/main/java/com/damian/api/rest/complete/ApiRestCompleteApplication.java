db91bdc35daf5959b46e13b86e60428be2f36d62
Damian Methol <damianmethol@gmail.com>
2022-06-14 18:17:05 -0300

    Initial commit setting up the structure of the project. Close #1

---
+++ api-rest-complete/src/main/java/com/damian/api/rest/complete/ApiRestCompleteApplication.java
@@ -0,0 +1,13 @@
+package com.damian.api.rest.complete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCompleteApplication.class, args);
+	}
+
+}
---
+++ api-rest-complete/src/test/java/com/damian/api/rest/complete/ApiRestCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damian.api.rest.complete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
